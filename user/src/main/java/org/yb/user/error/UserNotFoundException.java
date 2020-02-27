38538272101e7d0f28ad2277dbf16389c37e6019
YounesJonas <bouazizi.younes@gmail.com>
2020-02-27 22:13:57 +0100

    Delete UserNotFoundException.java

--- user/src/main/java/org/yb/user/error/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.yb.user.error;
-
-public class UserNotFoundException extends RuntimeException{
-
-	public UserNotFoundException(String id) {
-		super("this User is not found : " + id);
-	}
-}
